52736d0297c192ed3af838a2beeade67a8f3ee79
TRINADH KOYA <trinadhkoya9@gmail.com>
2016-12-27 09:22:33 +0530

    Update MainActivity.java

--- app/src/main/java/trinadhkoya/gihub/io/supportmultiplescreenlayout/MainActivity.java
+++ app/src/main/java/trinadhkoya/gihub/io/supportmultiplescreenlayout/MainActivity.java
@@ -11,24 +11,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
-//public class Test {
-//
-//    public static void main(String[] args) {
-//
-//
-//        double positiveMax = 60;
-//
-//        for (double dimen=300  ;dimen<=320;dimen+=30){
-//            for(double i=0.5;i<=positiveMax;i++){
-//                double ratio=i/300d;
-//                double sdp=ratio*dimen;
-//
-//                System.out.println("<dimen name="+"\"_" + i + "ssp\""+">"+sdp+"sp</dimen>");
-//
-//            }
-//        }
-//
-//
-//    }
-//
-//}
+//  public static void main(String[] args) {
+
+
+//         double positiveMax = 60;
+
+//         for (double dimen=300  ;dimen<=1080;dimen+=30){
+//             for(double i=0.5;i<=positiveMax;i++){
+//                 double ratio=i/300d;
+//                 double sdp=ratio*dimen;
+
+//                 System.out.println("<dimen name="+"\"_" + i + "ssp\""+">"+sdp+"sp</dimen>");
+
+//             }
+//         }
+
+
+//     }
